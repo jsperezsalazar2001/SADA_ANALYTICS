09845248514916329b9a33d68ef37e2947dce6a5
Naitsabes <jsperezsalazar2001@gmail.com>
2020-10-09 14:17:08 -0500

    Esto no funciona, bai

--- SADA_ANALYTICS/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-
-Route::get('/', 'HomeController@home')->name("home");
-
-Route::get('/raices','HomeController@calculoRaices')->name("raices");
-Route::POST('/busquedasIncrementales','HomeController@busquedasIncrementales')->name("busquedasIncrementales");
