4cb9647b93f89c10d7812f76d6177832a75d95ca
Yhoan Alejandro Guzman Garcia <yaguzmang@eafit.edu.co>
2020-10-11 00:17:27 -0500

    Added ES lang

---
+++ SADA_ANALYTICS/resources/lang/es/secant.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'title' => 'Secant method',
+    'input' => [
+        'f_function' => 'Ingrese f(x)',
+        'x1' => 'Ingrese x1',
+        'x2' => 'Ingrese x2',
+        'tolerance' => 'Ingrese la tolerancia',
+        'iterations' => 'Ingrese el máximo de iteraciones',
+    ],
+    'label' => [
+        'f_function' => 'f(x)',
+        'x1' => 'X1',
+        'x2' => 'X2',
+        'tolerance' => 'Tolerancia',
+        'iterations' => 'Iteraciones',
+    ],
+    'calculate' => 'Calcular',
+    'table' => [
+        'iteration' => 'Iteración',
+        'xi' => 'xi',
+        'f_xi' => 'f(xi)',
+        'error' => 'Error',
+    ],
+    'initial_data' => 'Datos inciales',
+    'success' => 'Exito: el método convergió',
+];
