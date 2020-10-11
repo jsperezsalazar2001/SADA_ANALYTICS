50dc477e1f08376b3ebf5e56eb080c49137fde18
Naitsabes <jsperezsalazar2001@gmail.com>
2020-10-11 15:36:39 -0500

    Adding pseudocodes folder

--- SADA_ANALYTICS/public/python/false_position.py
+++ SADA_ANALYTICS/public/python/false_position.py
@@ -23,42 +23,55 @@ err : error
 import sympy as sm
 import math
 import sys
+import traceback
+import json
+import base64
 
-def false_position(f, a, b, tol, n_max):
-    a = float(a)
-    b = float(b)
-    
-    results = []
-    x_in = sm.symbols('x')
-    
-    if n_max > 0:
-        f_a = sm.sympify(f).subs(x_in, a)
-        f_b = sm.sympify(f).subs(x_in, b) 
-        mp = (f_b*a - f_a*b)/(f_b - f_a)
-        f_mp = sm.sympify(f).subs(x_in, mp) 
-        E = 1000
-        cont = 1
-        results.append([cont, a, mp, b, f_mp, E])
+def false_position(f, a, b, tolerance, n_max):
+    results = {}
+    try:
+        f = str(f)
+        a = float(a)
+        b = float(b)
+        tolerance = float(tolerance)
+        n_max = int(n_max)
         
-        while E > tol and cont < n_max:
-            if f_a * f_mp < 0:
-                b = mp
-            else:
-                a = mp
-            
+        x_in = sm.symbols('x')
+        
+        if tolerance <= 0:
+            raise Exception("Tolerance must be positive")
+        if n_max > 0:
             f_a = sm.sympify(f).subs(x_in, a)
-            f_b = sm.sympify(f).subs(x_in, b)
-            p_0 = mp
+            f_b = sm.sympify(f).subs(x_in, b) 
             mp = (f_b*a - f_a*b)/(f_b - f_a)
             f_mp = sm.sympify(f).subs(x_in, mp) 
-            E = abs(mp - p_0)
-            cont = cont + 1
-            results.append([cont, a, mp, b, f_mp, E])
-    else:
-        print("Iterations number must be greater than 0")
-        sys.exit(1);
+            E = float("inf")
+            iter_count = 1
+            results[iter_count] = [int(iter_count), float(a), float(mp), float(b), float(f_mp), "N/A"]
+            
+            while E > tolerance and iter_count < n_max:
+                iter_count = iter_count + 1
+                if f_a * f_mp < 0:
+                    b = mp
+                else:
+                    a = mp
+                
+                f_a = sm.sympify(f).subs(x_in, a)
+                f_b = sm.sympify(f).subs(x_in, b)
+                p_0 = mp
+                mp = (f_b*a - f_a*b)/(f_b - f_a)
+                f_mp = sm.sympify(f).subs(x_in, mp) 
+                E = abs(mp - p_0)
+                results[iter_count] = [int(iter_count), float(a), float(mp), float(b), float(f_mp), float(E)]
+        else:
+            raise Exception("Iterations number must be greater than 0")
+    except BaseException as e:
+        results[0] = "Error in the given data: " + str(e)
         
-    for values in results:
-        print(values[4])
+    try:
+        aux = json.dumps(results)
+        print(aux)
+    except BaseException as e:
+        print("Error processing results: " + str(e))
 
 false_position("ln((sin(x)^2)+1)-(1/2)", 0, 1, 0.0000001, 100)
