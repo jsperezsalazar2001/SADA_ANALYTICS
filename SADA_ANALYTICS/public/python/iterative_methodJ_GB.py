14ecb9994c889373533c4196dfdec1605bec5525
Daniel <dfgomezm@eafit.edu.co>
2020-11-21 17:31:52 -0500

    Jacobi medio melo

--- SADA_ANALYTICS/public/python/iterative_methodJ_GB.py
+++ SADA_ANALYTICS/public/python/iterative_methodJ_GB.py
@@ -32,20 +32,21 @@ def solve_matrix2(a,b,matrix_type,x0,tol,Nmax):
         exit(1)
     if(matrix_function.determinant(a)):
         if (matrix_type == 'J'):
-            dic,dic_result = jacobi_method.jacobiMethod(l,d,u,b,x0,tol,Nmax)
+            dic,dic_result,C_aux,spectral_radius = jacobi_method.jacobiMethod(l,d,u,b,x0,tol,Nmax)
+            dic = matrix_function.rebuild_matrix(dic)
+            dic["C"] = C_aux
+            dic["spectral_radius"] = spectral_radius
         elif(matrix_type == 'GS'):
             dic,dic_result = gauss_seidel.gauss_seidel(l,d,u,b,x0,tol,Nmax)
         else:
             print("function type doesn't exist")
         
-        # for lop only for print result
-        print(dic["T"])
-        print(dic["C"])
-        print(dic["spectral_radius"])
-        for i in dic_result.keys():
-            print(dic_result[i])
+        print(json.dumps(dic))
+        print(json.dumps(dic_result))
+        
     else:
         print("function determinant is equals to 0")
         exit(1)
 
-x = solve_matrix2("[[4,-1,0,3],[1,15.5,3,8],[0,-1.3,-4,1.1],[14,5,-2,30]]","[1,1,1,1]",'J',"[0,0,0,0]","0.0000001","100")
+solve_matrix2(sys.argv[1],sys.argv[2],str(sys.argv[3]),sys.argv[4],sys.argv[5],sys.argv[6])
+#x = solve_matrix2("[[4,-1,0,3],[1,15.5,3,8],[0,-1.3,-4,1.1],[14,5,-2,30]]","[1,1,1,1]",'J',"[0,0,0,0]","0.0000001","100")
--- SADA_ANALYTICS/resources/views/iterativeMethodJGB.blade.php
+++ SADA_ANALYTICS/resources/views/iterativeMethodJGB.blade.php
@@ -19,8 +19,8 @@
             while (container_vector.hasChildNodes()) {
                 container_vector.removeChild(container_vector.lastChild);
             }
-            while (container_vectorx.hasChildNodes()) {
-                container_vectorx.removeChild(container_vectorx.lastChild);
+            while (container_vector_x.hasChildNodes()) {
+                container_vector_x.removeChild(container_vector_x.lastChild);
             }
             if (number>1) {
                 for (i=0;i<number;i++) {
@@ -36,7 +36,6 @@
                         input.required = true;
                         container_matrix.appendChild(input);
                     }
-                    //vector b
                     container_matrix.appendChild(document.createElement("br"));
                     container_matrix.appendChild(document.createElement("br"));
                     // Append a node with a random text
@@ -50,110 +49,141 @@
                     vector.required = true;
                     container_vector.appendChild(vector);
 
-                    // vector X0
-                    container_vector.appendChild(document.createElement("br"));
-                    container_vector.appendChild(document.createElement("br"));
+                    //container_vector.appendChild(document.createElement("br"));
+                    //container_vector.appendChild(document.createElement("br"));
                     // Append a node with a random text
                     container_vector_x.appendChild(document.createTextNode(""));
                     // Create an <input> element for vector B, set its type and name attributes
-                    var vector = document.createElement("input");
-                    vector.type = "number";
-                    vector.name = "vector_x" + i ;
-                    vector.style = "width : 110px;";
-                    vector.step = "any";
-                    vector.required = true;
-                    container_vector_x.appendChild(vector);
+                    var vector_aux = document.createElement("input");
+                    vector_aux.type = "number";
+                    vector_aux.name = "vector_x" + i ;
+                    vector_aux.style = "width : 110px;";
+                    vector_aux.step = "any";
+                    vector_aux.required = true;
+                    container_vector_x.appendChild(vector_aux);
                 }
                 document.getElementById("separador").style.display = 'block';
+                //document.getElementById("separador_x").style.display = 'block';
                 document.getElementById("matrix_a").style.display = 'block';
-                document.getElementById("vector_b").style.display = 'block';
-                document.getElementById("vector_x0").style.display = 'block'; 
+                document.getElementById("vector_b").style.display = 'block'; 
+                document.getElementById("vector_x").style.display = 'block'; 
                 document.getElementById("solve").style.display = 'block';
+                document.getElementById("additional_values").style.display = 'block';
             }
         }
     </script>
 </head>
-<div class="container matrix">
-    <div class="row justify-content-center sizeMatrix">
-        <div class="col-md-12">
-            <form method="POST" action="{{route('check_matrix')}}" class="form">
+<div class="container col-10" align="center">
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <form method="POST" action="{{route('iterative_j_g_b_values')}}" class="form">
                 @csrf
-                <div class="form-row">
+                <div class="form-row col-12" align="center">
                     <div class="form-group col-md-6">
-                        <label>{{ __('gaussian_method.dimension') }}</label>
+                        <label>{{ __('iterative_j_g_b_method.dimension') }}</label>
                         <input type="number" id="dimension" min="2" class="form-control" placeholder="Matrix dimension" name="n" step="any" required />
                     </div>
                         <!-- este div es el que pone feo la vista --> 
                     <div class="form-group col-md-6">
-                        <label> {{ __('gaussian_method.label.method_type') }} </label>
+                        <label> {{ __('iterative_j_g_b_method.label.method_type') }} </label>
                         <select name="method_type" class="form-control">
-                            <option value="J">{{ __('gaussian_method.input.simple_gaussian_method') }} J</option> 
-                            <option value="GS">{{ __('gaussian_method.input.partial_gaussian_method') }} GS</option>
-                            <option value="S">{{ __('gaussian_method.input.total_gaussian_method') }}</option>
+                            <option value="J"> {{ __('iterative_j_g_b_method.input.jacobi_method') }} </option> 
+                            <option value="GS">{{ __('iterative_j_g_b_method.input.gauss_seidel_method') }}</option>
                         </select>
                     </div>
-                </div>
-                <div class="form-row">
+                </div><br/>
+                <div class="form-row col-12 metodo" id="additional_values">
                     <div class="form-group col-md-6">
-                        <a id="filldetails" onclick="addFields()" class="btn btn-outline-primary btn-block">{{ __('gaussian_method.create_matrix') }}</a> 
+                        <label> {{ __('iterative_j_g_b_method.input.tolerance') }} </label>
+                        <input type="number" min="0" class="form-control" placeholder="{{ __('iterative_j_g_b_method.label.tolerance') }}" name="tolerance" step="any" required />
                     </div>
                     <div class="form-group col-md-6">
-                        <button id="solve" type="submit" class="btn btn-outline-success btn-block metodo">{{ __('gaussian_method.solve') }}</button> 
+                        <label> {{ __('iterative_j_g_b_method.input.iteration') }} </label>
+                        <input type="number" min="0" class="form-control" placeholder="{{ __('iterative_j_g_b_method.label.iteration') }}" name="iteration" required />
+                    </div>
+                </div><br/>
+                <div class="form-row col-12">
+                    <div class="form-group col-6">
+                        <a id="filldetails" onclick="addFields()" class="btn btn-outline-primary btn-block">{{ __('iterative_j_g_b_method.create_matrix') }}</a> 
+                    </div>
+                    <div class="form-group col-6">
+                        <button id="solve" type="submit" class="btn btn-outline-success btn-block metodo">{{ __('iterative_j_g_b_method.solve') }}</button> 
                     </div>
                 </div>
-                <div id="matrix_a" class="text-align metodo"> {{ __('gaussian_method.label.matrix_a') }} </div>
+                <div id="matrix_a" class="text-align metodo"> {{ __('iterative_j_g_b_method.label.matrix_a') }} </div>
                 <div id="matrix" class="text-align"> </div>
                 
-                <div id="separador" class="text-align metodo"> {{ __('gaussian_method.separator') }}</div>
-                <div id="vector_b" class="text-align metodo"> {{ __('gaussian_method.label.vector_b') }} </div>
+                <div id="separador" class="text-align metodo"> {{ __('iterative_j_g_b_method.separator') }}</div>
+                <div id="vector_b" class="text-align metodo"> {{ __('iterative_j_g_b_method.label.vector_b') }} </div>
                 <div id="vector" class="text-align"> </div>
 
-                <div id="separador" class="text-align metodo"> {{ __('gaussian_method.separator') }}</div>
-                <div id="vector_x0" class="text-align metodo"> {{ __('gaussian_method.label.vector_b') }} X </div>
+                <div id="vector_x" class="text-align metodo"> {{ __('iterative_j_g_b_method.label.vector_b') }} X </div>
                 <div id="vectorx" class="text-align"> </div>
-
-                <div class="form-group col-md-6">
-                    <label>{{ __('gaussian_method.dimension') }} X0</label>
-                    <input type="number" id="tolerance" min="0" class="form-control" placeholder="Matrix dimension" name="tolerance" step="any" required />
-                    <input type="number" id="iteration" min="1" class="form-control" placeholder="Matrix dimension" name="iteration" step="any" required />
-                </div>
             </form>
         </div>
-    </div>
-    @if ($data["solution"] == "true" and !empty($data["table"]))
-        <div class="card">
-            <div class="card-header">
-                <h1>Solution</h1>
-            <br>
-            </div>
-            <div class="card-body">
-                <div>
-                    @for ($i = 0; $i < count($data["table"])-1; $i++)
-                        <b><em>Step {{$i}}</em></b>
-                        $$A = \begin{pmatrix}
-                        @for ($j = 0; $j < count($data["table"][$i]); $j++)
-                            
-                            @for ($v = 0; $v < count($data["table"][$i][$j]); $v++)
-                                @if($v == count($data["table"][$i][$j])-1)
-                                    {{ $data["table"][$i][$j][$v]}} \\
+    </div><br/>
+    @if ($data["solution"] == "true" )
+        <div class="col-8">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Solution</h1>
+                <br>
+                </div>
+                <div class="card-body">
+                    <div>
+                        $$T = \begin{pmatrix}
+                        @for ($i = 0; $i < count($data["json_matrix"]["T"]); $i++)
+                                
+                            @for ($v = 0; $v < count($data["json_matrix"]["T"][$i]); $v++)
+                                @if($v == count($data["json_matrix"]["T"][$i])-1)
+                                    {{ $data["json_matrix"]["T"][$i][$v]}} \\
                                 @else
-                                    {{ $data["table"][$i][$j][$v]}} &
+                                    {{ $data["json_matrix"]["T"][$i][$v]}} &
                                 @endif
                             @endfor
-
+                            
                         @endfor
                         \end{pmatrix}$$
-                    @endfor
-                    
-                    $$X = \begin{bmatrix}
-                    @foreach ($data["table"]["x"] as $solution)
-                        @if($loop->last)
-                            {{$solution}}
-                        @else
-                            {{$solution}} \\
-                        @endif
-                    @endforeach
-                    \end{bmatrix}$$
+                        <br>
+                        $$C = \begin{bmatrix}
+                        @foreach ($data["json_matrix"]["C"] as $solution)
+                            @if($loop->last)
+                                {{$solution}}
+                            @else
+                                {{$solution}} & 
+                            @endif
+                        @endforeach
+                        \end{bmatrix}$$
+
+                        <br>
+
+                        $$\textbf {Spectral radius = } {{$data["json_matrix"]["spectral_radius"]}} $$
+
+                        <br>
+                        <table class="table table-striped" align="center">
+                            <thead>
+                                <tr>
+                                    <th>Iteration</th>
+                                    <th>Error</th>
+                                    <th colspan="{{$data['dimension']}}">Points</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @for ($i = 0; $i < count($data["json_table"]); $i++)
+                                    <tr>
+                                        <th>{{$data["json_table"][$i][0]}}</th>
+                                        <th>{{$data["json_table"][$i][1]}}</th>
+                                        @for ($j = 0; $j < count($data["json_table"][$i][2]); $j++)
+                                            <th>{{$data["json_table"][$i][2][$j]}}</th>
+                                        @endfor
+                                    </tr>
+                                    
+                                @endfor
+                            </tbody>
+                        </table>
+
+
+                    </div>
                 </div>
             </div>
         </div>
