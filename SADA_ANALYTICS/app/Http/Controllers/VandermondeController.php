5ef9e11622ae4da3f1c1c5dad724eb733626ef13
Naitsabes <jsperezsalazar2001@gmail.com>
2020-11-23 14:12:57 -0500

    Updating dockerfile ... again ... again ... again

--- SADA_ANALYTICS/app/Http/Controllers/VandermondeController.php
+++ SADA_ANALYTICS/app/Http/Controllers/VandermondeController.php
@@ -27,13 +27,13 @@ class VandermondeController extends Controller
         $Arrx = json_encode($Arrx);
         $Arry = json_encode($Arry);
 
-        #$command = 'python "'.public_path().'\python\vandermonde.py" '." ".$Arrx." ".$Arry;
-        #$command = escapeshellcmd('python3.6 -V');
-        #$output = explode("\n", substr_replace(shell_exec($command),"",-2));
-        $command = 'python3 "'.public_path().'\python\vandermonde.py" '." ".$Arrx." ".$Arry;
-        exec($command, $output);
+        $command = escapeshellcmd('python3 "'.public_path().'\python\vandermonde.py" '." ".$Arrx." ".$Arry);
+        #$command = escapeshellcmd('python3 -V');
+        $output = explode("\n", substr_replace(shell_exec($command),"",-2));
+        #$command = 'python3 "'.public_path().'\python\vandermonde.py" '." ".$Arrx." ".$Arry;
+        #exec($command, $output);
         $data = [];
-        #dd($output);
+        dd($output);
         $data["title"] = __('vandermonde_method.title');
         if (substr($output[0],7,5) == "Error"){
             $data["solution"] = "false";
