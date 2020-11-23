d057e3576dc8efc87d054e8e226247ae9685b3da
Naitsabes <jsperezsalazar2001@gmail.com>
2020-11-23 15:12:02 -0500

    Updating dockerfile ... again ... again ... again

--- SADA_ANALYTICS/app/Http/Controllers/VandermondeController.php
+++ SADA_ANALYTICS/app/Http/Controllers/VandermondeController.php
@@ -27,13 +27,15 @@ class VandermondeController extends Controller
         $Arrx = json_encode($Arrx);
         $Arry = json_encode($Arry);
 
-        $command = escapeshellcmd('python3 "'.public_path().'\python\vandermonde.py" '." ".$Arrx." ".$Arry);
+        $command = 'python3 "'.public_path().'\python\vandermonde.py" '." ".$Arrx." ".$Arry;
         #$command = escapeshellcmd('python3 -V');
+
         $output = explode("\n", substr_replace(shell_exec($command),"",-2));
+        exec($command, $output2);
         #$command = 'python3 "'.public_path().'\python\vandermonde.py" '." ".$Arrx." ".$Arry;
         #exec($command, $output);
         $data = [];
-        dd($command);
+        dd($command, $output, $output2);
         $data["title"] = __('vandermonde_method.title');
         if (substr($output[0],7,5) == "Error"){
             $data["solution"] = "false";
